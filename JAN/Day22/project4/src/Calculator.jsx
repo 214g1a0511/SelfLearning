d224ebde727d6a8c2a0dc91489284db3d3d260df
214g1a0511 <214g1a0511@srit.ac.in>
2025-01-22 17:32:02 +0530

    new layout

--- JAN/Day22/project4/src/Calculator.jsx
+++ JAN/Day22/project4/src/Calculator.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import "./App.css";
 import History from "./History";
-import bs from "./assets/bs.png"
+import bs from "./assets/backspace.png"
 
 const Calculator = () => {
   const [display, setDisplay] = useState("");
@@ -20,10 +20,12 @@ const Calculator = () => {
 
   const handleClear = () => {
     setDisplay("");
+    //console.log("cleared")
   };
 
   const handleDelete = () => {
     setDisplay(display.slice(0, -1));
+    //console.log("deleteed")
   };
 
   const handleEvaluate = () => {
@@ -48,11 +50,11 @@ const Calculator = () => {
   return (
     <animated.div
       style={fadeIn}
-      className="container col-12 col-sm-8 col-md-6 col-lg-4 my-4 p-5 border rounded shadow-lg bg-light"
+      className="container col-12 col-sm-8 col-md-6 col-lg-4 my-4 p-5 border rounded shadow-lg bg-dark"
     >
       <div className="row">
         <div className="sm-col-12 md-col-5">
-          <div className="display p-3 bg-dark text-white text-end rounded mb-3">
+          <div className="display p-4 fs-1 bg-dark text-white text-end rounded mb-3">
             {display}
           </div>
         </div>
@@ -60,75 +62,40 @@ const Calculator = () => {
           <div className="row">
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("1")}
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={handleClear}
               >
-                1
+                AC
               </button>
             </div>
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("2")}
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("%")}
               >
-                2
+                %
               </button>
             </div>
             <div className="col-3 mb-2">
-              <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("3")}
-              >
-                3
-              </button>
+              <div class="rounded-circle bg-secondary p-3">
+              <img src={bs} width="48px"alt="clear"
+              onClick={handleDelete}
+              />
+              </div>
             </div>
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("+")}
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("/")}
               >
-                +
+                /
               </button>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-3 mb-2">
-              <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("4")}
-              >
-                4
-              </button>
             </div>
+            <div className="row">
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("5")}
-              >
-                5
-              </button>
-            </div>
-            <div className="col-3 mb-2">
-              <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("6")}
-              >
-                6
-              </button>
-            </div>
-            <div className="col-3 mb-2">
-              <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("-")}
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-3 mb-2">
-              <button
-                className="btn btn-secondary w-100 fs-2"
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
                 onClick={() => handleClick("7")}
               >
                 7
@@ -136,7 +103,7 @@ const Calculator = () => {
             </div>
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
                 onClick={() => handleClick("8")}
               >
                 8
@@ -144,7 +111,7 @@ const Calculator = () => {
             </div>
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
                 onClick={() => handleClick("9")}
               >
                 9
@@ -152,62 +119,116 @@ const Calculator = () => {
             </div>
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
                 onClick={() => handleClick("*")}
               >
-                *
+                X
               </button>
             </div>
-          </div>
-          <div className="row">
+            </div>
+            <div className="row">
             <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("4")}
+              >
+                4
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("5")}
+              >
+                5
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("6")}
+              >
+                6
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("-")}
+              >
+                -
+              </button>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("1")}
+              >
+                1
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("2")}
+              >
+                2
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("3")}
+              >
+                3
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("+")}
+              >
+                +
+              </button>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick("00")}
+              >
+                00
+              </button>
+            </div>
+            <div className="col-3 mb-2">
+              <button
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
                 onClick={() => handleClick("0")}
               >
                 0
               </button>
             </div>
             <div className="col-3 mb-2">
-              <button className="btn btn-secondary w-100 fs-2" onClick={handleClear}>
-                C
-              </button>
-            </div>
-            <div className="col-3 mb-2">
-             <img
-                     src={bs} 
-                     alt="Clear"
-                     style={{ width: '95px', height: '65px', cursor: 'pointer', marginBottom: '10px' }}
-                     onClick={handleDelete}
-                   />
-            </div>
-            <div className="col-3 mb-2">
               <button
-                className="btn btn-secondary w-100 fs-2"
-                onClick={() => handleClick("/")}
+                className="btn btn-secondary w-100 fs-2 rounded-circle p-3"
+                onClick={() => handleClick(".")}
               >
-                /
+                .
               </button>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-4 mb-2 m-auto">
+            <div className="col-3 mb-2">
               <button
-                className="btn btn-warning w-100 fs-2"
+                className="btn btn-warning w-100 fs-2 rounded-circle p-3"
                 onClick={handleEvaluate}
               >
                 =
               </button>
             </div>
-          </div>
-        </div>
-        <div className="col-12">
-          <button className="btn btn-info w-100 mt-3" onClick={toggleHistory}>
-            {isHistoryVisible ? "Hide History" : "Show History"}
-          </button>
-          {isHistoryVisible && <History history={history} clearHistory={handleClearHistory}  />}
-          
-        </div>
+            </div>
+            </div>
         
       </div>
     </animated.div>
