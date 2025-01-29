8a7a9f0d34dc0b6c4d07133286c757554f584547
214g1a0511 <214g1a0511@srit.ac.in>
2025-01-29 12:48:27 +0530

    tictactoe

---
+++ JAN/Day29/tic-tac-toe/src/components/TicTacToe.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./TicTacToe.css";
+import winSound from '../assets/victorymale-version-230553.mp3'
+import clickSound from "../assets/sound-1-167181.mp3"
+const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const win=new Audio(winSound)
+  const click=new Audio(clickSound)
+
+  const checkWinner = (squares) => {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (let [a, b, c] of winningCombinations) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        win.play()
+        return squares[a];
+
+      }
+    }
+    return null;
+  };
+
+  const handleClick = (index) => {
+    click.play()
+    if (board[index] || checkWinner(board)) return;//to check if already the cell is occupied or game is already over
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? "X" : "O";
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const winner = checkWinner(board);
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setIsXNext(true);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Tic-Tac-Toe</h1>
+      <div className="board">
+        {board.map((cell, index) => (
+          <button
+            key={index}
+            onClick={() => handleClick(index)}
+            className="cell"
+          >
+            {cell}
+          </button>
+        ))}
+      </div>
+      {winner && <p className="winner">Winner: {winner}</p>}
+      <button onClick={resetGame} className="reset-button">Reset</button>
+    </div>
+  );
+};
+
+export default TicTacToe;
